a2d01d59df11bf73df655a49fec18c22d613205b
Ilya Lyalin <lyalin315@gmail.com>
2022-12-01 02:37:23 +0300

    dz1

---
+++ zadanie1/Program.cs
@@ -0,0 +1,6 @@
+﻿//можно вводить число вручную или брать рандомное
+//int numA = Convert.ToInt32(Console.ReadLine());
+int numA = new Random().Next(100, 1000);
+Console.WriteLine($"Ваше число {numA}");
+int n = numA%100/10;
+Console.WriteLine($"{n}");
